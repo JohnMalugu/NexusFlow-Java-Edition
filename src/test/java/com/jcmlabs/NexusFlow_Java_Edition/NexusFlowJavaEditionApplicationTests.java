c887247f24491fdf34a0593b1245d316ba057211
JohnMalugu <johnmalugu99@gmail.com>
2026-01-09 17:37:54 +0300

    feat: initial project setup with Java 21, Spring Boot 4, and Gradle

---
+++ src/test/java/com/jcmlabs/NexusFlow_Java_Edition/NexusFlowJavaEditionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcmlabs.NexusFlow_Java_Edition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusFlowJavaEditionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
