64d9072c1993a9ddedaafe16c3efe06613bb6161
JohnMalugu <johnmalugu99@gmail.com>
2026-01-11 21:17:45 +0300

    in memory task repo

---
+++ src/main/java/com/jcmlabs/NexusFlow_Java_Edition/domain/workflow/SubmitWorkflowUseCase.java
@@ -0,0 +1,56 @@
+package com.jcmlabs.NexusFlow_Java_Edition.domain.workflow;
+
+
+import com.jcmlabs.NexusFlow_Java_Edition.domain.task.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.streams.processor.internals.Task;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
+
+@Service
+@RequiredArgsConstructor
+public class SubmitWorkflowUseCase {
+
+    private final WorkflowRepository workflowRepository;
+    private final TaskRepository taskRepository;
+
+    @Transactional
+    public UUID execute(SubmitWorkflowCommand command) {
+
+        Workflow workflow = Workflow.create(command.name());
+
+        List<Task> domainTasks = command.tasks().stream()
+                .map(def -> Task.newTask(
+                        def.name(),
+                        Task.TaskType.valueOf(def.type().toUpperCase()),
+                        def.payload()
+                ))
+                .toList();
+
+        domainTasks.forEach(workflow::addTask);
+
+        if (command.dependencies() != null) {
+            for (var dep : command.dependencies()) {
+                validateIndices(dep, domainTasks.size());
+                Task from = domainTasks.get(dep.fromIndex());
+                Task to = domainTasks.get(dep.toIndex());
+                workflow.addDependency(from, to);
+            }
+        }
+
+        workflow.getReadyTasks().forEach(Task::schedule);
+
+        workflowRepository.save(workflow);
+        taskRepository.saveAll(domainTasks);
+
+        return workflow.getId();
+    }
+
+    private void validateIndices(SubmitWorkflowCommand.DependencyDefinition dep, int size) {
+        if (dep.fromIndex() >= size || dep.toIndex() >= size || dep.fromIndex() < 0 || dep.toIndex() < 0) {
+            throw new IllegalArgumentException("Dependency index out of bounds");
+        }
+    }
+}
---
+++ src/main/java/com/jcmlabs/NexusFlow_Java_Edition/infrastructure/persistence/InMemoryTaskRepository.java
@@ -0,0 +1,19 @@
+package com.jcmlabs.NexusFlow_Java_Edition.infrastructure.persistence;
+
+import com.jcmlabs.NexusFlow_Java_Edition.domain.task.Task;
+import com.jcmlabs.NexusFlow_Java_Edition.domain.task.TaskRepository;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class InMemoryTaskRepository implements TaskRepository {
+
+    private final Map<UUID, Task> store = new ConcurrentHashMap<>();
+
+    @Override
+    public void saveAll(Collection<Task> tasks) {
+        tasks.forEach(task -> store.put(task.getId(), task));
+    }
+}
